d077e201f5891bfd0ec27c88078198265f2d295c
shailstar <shailstar201595@gmail.com>
2021-07-16 21:52:16 +0530

    fdf

--- laraweb/settings.py
+++ laraweb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jx$0^um83p8slbd)87=4(*6rj6)o%#)(j#ydzuvmn7(4jf%$*-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['lara-make.herokuapp.com', '127.0.0.1']
 
